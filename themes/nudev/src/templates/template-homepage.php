ac16587022f9663557a8ba9eb74cc473d9cf8cb8
Craig <info@dubhelix.com>
2018-05-21 06:39:34 -0400

    Add files via upload

---
+++ themes/nudev/src/templates/template-homepage.php
@@ -0,0 +1,27 @@
+<?php
+
+	/* Template Name: Homepage */
+
+
+
+
+	get_header();
+
+?>
+
+	<main id="nu__homepage" role="main" aria-label="content" class="smooth">
+
+		<div id="nu__seo-content"><h1><?=get_the_content()?></h1></div>
+
+		<div id="nu__stories">
+
+			<div id="prev" title="Click here to go to the previous slide">&#xE5C4;</div>
+		  <div id="next" title="Click here to go to the next slide">&#xE5C8;</div>
+
+			<?php get_template_part('loops/loop-homepagestories'); ?>
+
+		</div>
+
+	</main>
+
+<?php get_footer(); ?>
