cc1827b10ab38efd2974ed7e89d1fd384a1ec0db
Craig <info@dubhelix.com>
2018-06-12 11:49:41 -0400

    Add files via upload

---
+++ themes/nudev/src/templates/template-interior.php
@@ -0,0 +1,17 @@
+<?php
+
+	/* Template Name: Interior */
+
+
+
+
+	get_header();
+
+?>
+
+	<main id="nu__interior" role="main" aria-label="content" class="smooth">
+        how are you
+
+	</main>
+
+<?php get_footer(); ?>
